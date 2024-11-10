a69d21821da10e366d511a21bd155ea4d1399512
Faridul Islam <faridbpi10@gmail.com>
2024-11-10 14:45:30 +0600

    image hotspot slider html

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     // Thumbnail Slider
     var thumbSlider = new Swiper(".thumb-slider", {
-      slidesPerView: 3, // Number of thumbnails visible
-      watchSlidesProgress: true,
+        slidesPerView: 3, // Number of thumbnails visible
+        watchSlidesProgress: true,
     });
-  
+
     // Main Slider with Thumbnail Control
     var mainSlider = new Swiper(".main-slider", {
         slidesPerView: 1, // Number of slides visible
-      spaceBetween: 10,
-      thumbs: {
-        swiper: thumbSlider,
-      },
+        spaceBetween: 10,
+        thumbs: {
+            swiper: thumbSlider,
+        },
     });
-  });
+    // Initialize Tippy tooltips
+    tippy('.thumb-slider .swiper-slide', {
+        placement: 'top',       // Tooltip position
+        theme: 'light',         // Light theme for tooltip
+        animation: 'scale',     // Tooltip animation
+        duration: 200           // Animation duration
+    });
+});
